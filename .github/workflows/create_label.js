a709bef7ee3e8d8fe439a85a2d70e34803cdcdf3
Mupu <Mupuk55@web.de>
2024-08-08 12:05:40 +0200

    Create create_label.js

---
+++ .github/workflows/create_label.js
@@ -0,0 +1,36 @@
+module.exports = async ({github, labelName}) => {
+  // TEMP
+  // Specify the Berlin time zone and desired format
+  let options = {
+    timeZone: 'Europe/Berlin',
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+  };
+  // Format the date and time
+  let berlinTime = new Intl.DateTimeFormat('en-CA', options).format(new Date());
+  // Extract the date part (YYYY-MM-DD)
+  let berlinDate = berlinTime.split(',')[0];
+  console.log(berlinTime);
+  // TEMP
+  
+  // Check if the label exists
+  const { data: labels } = await github.rest.issues.listLabelsForRepo({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+  });
+
+  const labelExists = labels.some(label => label.name === labelName);
+
+  // If the label doesn't exist, create it
+  if (!labelExists) {
+    await github.rest.issues.createLabel({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      name: jai_version,
+    });
+  }
+}
