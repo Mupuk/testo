2d4dfc0264dfbaf2f86dd1e410aa683bedaf724b
Mupu <Mupuk55@web.de>
2024-08-08 20:57:56 +0200

    a

---
+++ .github/workflows/handle_issue_command.js
@@ -0,0 +1,50 @@
+function parseIssueBody(issueBody) {
+  // Split the input text into lines
+  const lines = issueBody.split('\n');
+  let pipeLines = lines.filter(line => line.startsWith('|'));
+  // Discard the first two lines (header and table)
+  pipeLines = pipeLines.slice(2);
+  console.log(pipeLines);
+
+  // Extract the row that contains the variable values
+  const regex = /\|?(.*?)\|/gm;
+  const fields = [...pipeLines[0].matchAll(regex)].map(match => match[1].trim());
+
+  return fields
+}
+
+const handleEmailedIn = async ({ github, context }) => {
+
+  const { data: issue } = await github.rest.issues.get({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: context.issue.number
+  });
+
+  const parsedFields = parseIssueBody(issue.body);
+  console.log("Res");
+  console.log(parsedFields);
+
+  let modifiedRow = '|' + parsedFields.join('|') + '|';
+
+  // Reassamble updated post
+  let lines = issue.body.split('\n');
+
+  // find data row
+  let headerIndex = -1;
+  for (const [index, line] of lines.entries()) {
+    if (line.startsWith('|')) {
+      headerIndex = index;
+      break;
+    }
+  }
+
+  console.log(lines[headerIndex + 2]);
+  lines.splice(headerIndex + 2, 1, modifiedRow);
+
+  console.log(lines.join('\n'));
+}
+
+module.exports = {
+  handleEmailedIn,
+};
