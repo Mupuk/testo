fa5a0cf6e461ca2c36fc5b57dbc3ffcf740e17dd
Mupu <Mupuk55@web.de>
2024-08-09 22:41:35 +0200

    a

--- .github/workflows/bug_suit.js
+++ .github/workflows/bug_suit.js
@@ -1,5 +1,48 @@
-const createIssue = async ({github, context, exec}) => {
-  await exec.exec('jai bug_suit.jai');
+const decrementVersionString = (version) =>  {
+  const versionRegex = /(beta-)(\d+).(\d+).(\d+)/
+  const versionSplit = version.match(versionRegex);
+
+  // Decrement version
+  let carry = 0;
+  let newMicro = parseInt(versionSplit[4]) - 1;
+  if (newMicro < 0) {
+    carry = 1
+    newMicro = 0;
+  }
+  let newMinor = parseInt(versionSplit[3]) - carry;
+  if (newMinor < 0) {
+    carry = 1
+    newMinor = 0;
+  }
+  let newMajor = parseInt(versionSplit[2]) - carry;
+  if (newMajor < 0) {
+    newMajor = 0;
+  }
+  return `${versionSplit[1]}${newMajor}.${newMinor}.${newMicro}`
+}
+
+const createIssue = async ({github, context, exec, io}) => {
+  const { OS } = process.env;
+  console.log(OS);
+
+  const test = await io.which('jai')
+  console.log(test);
+
+  const { jaiVersion: get_jai_version } = require('./utils.js');
+  let currentVersion = await get_jai_version();
+
+  let compiler_path = 'jai'; // we start with the current one
+  await exec.exec(`${compiler_path} bug_suit.jai`);
+
+  currentVersion = decrementVersionString(currentVersion);
+  compiler_path = 'c:/' + '/bin/jai';
+  await exec.exec(`${compiler_path} bug_suit.jai`);
+
+  currentVersion = decrementVersionString(currentVersion);
+  compiler_path = 'c:/' + '/bin/jai';
+  await exec.exec(`${compiler_path} bug_suit.jai`);
+
+
   
   let content = {};
 
