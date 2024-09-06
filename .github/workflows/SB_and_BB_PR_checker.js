99108ddce1bf442abf52ae0b21a1c4c1fdd05f35
Mupu <Mupuk55@web.de>
2024-09-07 01:34:14 +0200

    .

--- .github/workflows/SB_and_BB_PR_checker.js
+++ .github/workflows/SB_and_BB_PR_checker.js
@@ -1,4 +1,17 @@
 const SBAndBBPRChecker = async ({ github, context, exec, io }) => {
+     const { data: pr } = await github.rest.pulls.get({
+        ...context.repo,
+        pull_number: context.issue.number
+    });
+
+    await _SBAndBBPRChecker({github, pr});
 };
 
-module.exports = SBAndBBPRChecker;
+const _SBAndBBPRChecker = async ({ github, pr }) => {
+    console.log(pr);
+};
+
+module.exports = {
+    SBAndBBPRChecker,
+    _SBAndBBPRChecker,
+};
