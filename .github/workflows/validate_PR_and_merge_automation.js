ac687bd1a102cbd0f2ba0b4a013be18e062c2a62
Mupu <Mupuk55@web.de>
2024-09-08 17:27:27 +0200

    .

--- .github/workflows/validate_PR_and_merge_automation.js
+++ .github/workflows/validate_PR_and_merge_automation.js
@@ -109,7 +109,7 @@ const validateAddedTestAndMergeOnSuccess = async ({ github, exec, io, contextRep
   // Git commands to add, commit, and push changes
   await exec.exec('git', ['config', 'user.name', 'github-actions[bot]']);
   await exec.exec('git', ['config', 'user.email', 'github-actions[bot]@users.noreply.github.com']);
-  await exec.exec('git', ['add --all']);
+  await exec.exec('git', ['add', '--all']);
   await exec.exec('git', ['commit', '-m', 'Updated file content via GitHub Actions']);
   await exec.exec('git', ['push']);
 
