e2b02a3bc6eb5e1036ba66cce7cd1ab17cfe307f
Mupu <Mupuk55@web.de>
2024-09-09 22:00:02 +0200

    .

--- .github/workflows/run_test_suit_and_update.js
+++ .github/workflows/run_test_suit_and_update.js
@@ -268,7 +268,7 @@ const runTestSuitAndUpdate = async ({ github, context, exec, io }) => {
       }, {})[platform];
       console.log('lastHistoryEntryOfPCurrentlatform', lastHistoryEntryOfPCurrentlatform);
 
-    const testToggled = (lastHistoryEntryOfPCurrentlatform.passed_test === false && currentTest.passed_test === true) || (lastHistoryEntryOfPCurrentlatform.passed_test === true && currentTest.passed_test === false);
+    const testToggled = (lastHistoryEntryOfPCurrentlatform.passed_test === '❌' && currentTest.passed_test === true) || (lastHistoryEntryOfPCurrentlatform.passed_test === '✅' && currentTest.passed_test === false);
 
     let replaceIndex = 0;
     newCommentBody = newCommentBody.replace(parseIssueHistoryRegex, (match, passedTest, platforms, date, oldVersion, errorCode, expectedErrorCode, i) => {
