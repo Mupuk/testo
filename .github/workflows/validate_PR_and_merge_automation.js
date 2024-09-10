55097f8af12eb2422ee46c7c5bcdc2a4f7cc2873
Mupu <Mupuk55@web.de>
2024-09-10 15:33:21 +0200

    .

--- .github/workflows/validate_PR_and_merge_automation.js
+++ .github/workflows/validate_PR_and_merge_automation.js
@@ -115,6 +115,8 @@ const validateAddedTestAndMergeOnSuccess = async ({ github, exec, io, contextRep
     await io.mv(oldFolderName, newFolderName);
   }
 
+  await exec.exec('git rev-parse HEAD');
+
   // Git commands to add, commit, and push changes
   await exec.exec('git', ['config', 'user.name', 'github-actions[bot]']);
   await exec.exec('git', ['config', 'user.email', 'github-actions[bot]@users.noreply.github.com']);
@@ -123,11 +125,18 @@ const validateAddedTestAndMergeOnSuccess = async ({ github, exec, io, contextRep
   await exec.exec('git', ['commit', '-m', 'Updated file paths to match tracking issue number']);
   await exec.exec('git', ['push']);
   
+  await exec.exec('git rev-parse HEAD');
   // Not sure why the merge below only works after Checking out the PR again.
   // If we dont do this, the merge will fail with 'Head branch is out of date'
   // Currently, we work around this by checking out the PR branch again
 
   console.log('trying to merge')
+
+  const { data: pr2 } = await github.rest.pulls.get({
+    ...contextRepo,
+    pull_number: prNumber
+  });
+  console.log('pr2', pr2);
 
   const mergeResponse = await github.rest.pulls.merge({
     ...contextRepo,
