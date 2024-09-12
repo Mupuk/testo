5cff3ec1c47b2ed1af126150e11ce7221c17e065
Mupu <Mupuk55@web.de>
2024-09-12 15:54:15 +0200

    .

--- .github/workflows/run_test_suit_and_update.js
+++ .github/workflows/run_test_suit_and_update.js
@@ -385,7 +385,7 @@ const runTestSuitAndUpdate = async ({ github, context, exec, io }) => {
 
 
   // @todo instead up update here, pass result to updater
-  const testSuitOutput = { test: 'wor\'king ' + platform };
+  const testSuitOutput = { test: 'wor\'k\"in\`g ' + platform };
   return testSuitOutput;
 };
 
