a302cf98f65814e9f3e9a5e68e0b7d6ffb2f0bbe
Mupu <Mupuk55@web.de>
2024-12-03 12:05:12 +0100

    .

--- .github/workflows/3_run_test_suit_and_update.js
+++ .github/workflows/3_run_test_suit_and_update.js
@@ -785,10 +785,8 @@ const updateGithubIssuesAndFiles = async ({
       for (const version of allTestResultVersions) {
         let row = fullHistoryDataByVersion[version];
         if (!row) { // insert new row
-          console.log('Insert new row', issueNumber, version);
-          fullHistoryDataByVersion[version] = { };
+          fullHistoryDataByVersion[version] = {};
           row = fullHistoryDataByVersion[version]; 
-          // row = fullHistoryDataByVersion[version] == {version};
           getGroupNames(parseIssueHistoryRegex).forEach(groupName => {
             if (groupName === 'version') { // Special case for version
               row[groupName] = version;
