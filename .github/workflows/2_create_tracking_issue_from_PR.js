271a4fddba20f72ddfc0bb60a7dc7d624b6ae788
Mupu <Mupuk55@web.de>
2024-12-07 00:18:00 +0100

    .

--- .github/workflows/2_create_tracking_issue_from_PR.js
+++ .github/workflows/2_create_tracking_issue_from_PR.js
@@ -129,7 +129,7 @@ const renameAllFilesToMatchTracker = async ({ github, context, originalPRData, v
   });
 
   // Update the tree by renaming files of this PR to match the tracker issue number
-  const validBugNameRegexTemplate = `^compiler_bugs/${context.issue.number}_{TRACKERNUMBER}_[CR]EC-?\\d+(?:\\.jai$|/)`; // @copyPasta
+  const validBugNameRegexTemplate = `^compiler_bugs/{TRACKERNUMBER}_${context.issue.number}_[CR]EC-?\\d+(?:\\.jai$|/)`; // @copyPasta
   // When running the first time, the trackerIssueNumber is 0, so we need to replace it with the actual number
   const validBugNameNoTrackerRegex = new RegExp(validBugNameRegexTemplate.replace('{TRACKERNUMBER}', 0));
   const validBugNameAnyTrackerRegex = new RegExp(validBugNameRegexTemplate.replace('{TRACKERNUMBER}', '\\d+'));
