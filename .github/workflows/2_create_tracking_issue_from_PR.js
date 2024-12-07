50a6a983b86f285c815e34ac4c6d1bc8e147611d
Mupu <Mupuk55@web.de>
2024-12-07 15:56:29 +0100

    .

--- .github/workflows/2_create_tracking_issue_from_PR.js
+++ .github/workflows/2_create_tracking_issue_from_PR.js
@@ -114,7 +114,7 @@ const createTrackingIssueFromPR = async ({ github, context, originalPRData }) =>
   await github.rest.issues.createComment({
     ...context.repo,
     issue_number: issue.number,
-    body: `👋 Thanks for the contribution @${originialIssueCreator}. If you want to get notified when this issue is fixed, or breaks again, subscribe to this issue!`,
+    body: `👋 Thanks for the contribution @${originialIssueCreator}. This is the tracker for the PR #${context.issue.number}. If you want to get notified when this issue is fixed, or breaks again, subscribe to this issue!`,
   });
 
   return issue.number;
